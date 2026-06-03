6f065f842445dd19f30c3972a417cd46916c22da
amtoha-prog <amtoha@alustudent.com>
2026-06-03 11:48:25 -0700

    Added description comments

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,9 +1,14 @@
-#/!bin/bash
+#!/usr/bin/env bash
 
+#Member 1 - The Architect
+
+# Directory names as variables
 ACTIVE_DIR="active_logs"
 ARCHIVE_DIR="archived_logs"
 REPORTS_DIR="reports"
 
+
+#Function checks if the required directories exist if not it creates them
 initialize_system() {
 	echo "Initializing"
 	for dir in "$ACTIVE_DIR" "$ARCHIVE_DIR" "$REPORTS_DIR"; do
