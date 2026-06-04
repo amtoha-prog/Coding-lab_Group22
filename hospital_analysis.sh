522171d65ba77d1903ac34eeeb2ac5a10fe18f6a
mwanikifelix-01 <f.mwaniki@alustudent.com>
2026-06-04 06:58:08 -0700

    summary

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -19,3 +19,23 @@ water_audit() {
 		echo "Error: $WATER_LOG not found"
 		return 1
 	fi
+	
+
+	awk '
+            BEGIN { FS=" [|] " }
+	    $2 == "ICU_WATER_RESERVE" {
+	    total += $3
+	    readings++
+	    if ($3 > peak) peak = $3
+           }
+       END {
+           printf "Usage Summary"
+           if (readings > 0) {
+	      printf " Readings analysed : %d\n", readings
+	      printf " Average usage     : %.2f Litres/min\n", total / reading    
+	      printf " Peak usage        : %d Litres/min\n", peak
+           } else{
+               printf " No ICU_WATER_RESERVE data found.\n"
+           }     
+      }
+    ' "$WATER_LOG"
